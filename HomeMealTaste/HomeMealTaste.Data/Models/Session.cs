56874ebbc51e4a90b5c99f4b2c0e0fcc84f93d50
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-07 09:58:16 +0700

    modify model

--- HomeMealTaste/HomeMealTaste.Data/Models/Session.cs
+++ HomeMealTaste/HomeMealTaste.Data/Models/Session.cs
@@ -5,6 +5,14 @@ namespace HomeMealTaste.Data.Models
 {
     public partial class Session
     {
+        public Session()
+        {
+            Areas = new HashSet<Area>();
+            Groups = new HashSet<Group>();
+            MealSessions = new HashSet<MealSession>();
+            Orders = new HashSet<Order>();
+        }
+
         public int SessionId { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? StartTime { get; set; }
@@ -14,5 +22,12 @@ namespace HomeMealTaste.Data.Models
         public int? UserId { get; set; }
         public bool? Status { get; set; }
         public string? SessionType { get; set; }
+
+        public virtual User? User { get; set; }
+        public virtual ICollection<Area> Areas { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+        public virtual ICollection<MealSession> MealSessions { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
+
