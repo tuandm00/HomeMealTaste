5ac2effd0cb54a798aa11779821c815a82a3adf1
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-07 10:05:02 +0700

    modify models

--- HomeMealTaste/HomeMealTaste.Data/Models/Meal.cs
+++ HomeMealTaste/HomeMealTaste.Data/Models/Meal.cs
@@ -5,10 +5,22 @@ namespace HomeMealTaste.Data.Models
 {
     public partial class Meal
     {
+        public Meal()
+        {
+            MealDishes = new HashSet<MealDish>();
+            MealSessions = new HashSet<MealSession>();
+            Orders = new HashSet<Order>();
+        }
+
         public int MealId { get; set; }
         public string? Name { get; set; }
         public string? Image { get; set; }
         public decimal? DefaultPrice { get; set; }
         public int? KitchenId { get; set; }
+
+        public virtual Kitchen? Kitchen { get; set; }
+        public virtual ICollection<MealDish> MealDishes { get; set; }
+        public virtual ICollection<MealSession> MealSessions { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
