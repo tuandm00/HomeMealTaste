7eb94e85628216d6c25736eff470dfab94c80f5c
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-07 11:24:10 +0700

    q4

--- HomeMealTaste/HomeMealTaste.Data/Models/Customer.cs
+++ HomeMealTaste/HomeMealTaste.Data/Models/Customer.cs
@@ -5,6 +5,15 @@ namespace HomeMealTaste.Data.Models
 {
     public partial class Customer
     {
+        public Customer()
+        {
+            Feedbacks = new HashSet<Feedback>();
+            Groups = new HashSet<Group>();
+            Memberships = new HashSet<Membership>();
+            Orders = new HashSet<Order>();
+            Payments = new HashSet<Payment>();
+        }
+
         public int CustomerId { get; set; }
         public int? UserId { get; set; }
         public string? Name { get; set; }
@@ -12,5 +21,12 @@ namespace HomeMealTaste.Data.Models
         public string? Phone { get; set; }
         public bool? AccountStatus { get; set; }
         public string? District { get; set; }
+
+        public virtual User? User { get; set; }
+        public virtual ICollection<Feedback> Feedbacks { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+        public virtual ICollection<Membership> Memberships { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
     }
 }
