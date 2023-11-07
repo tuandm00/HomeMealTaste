4e7114993acd8a0ecd418fc78d19b5914eb123a7
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-07 10:17:13 +0700

    b

--- HomeMealTaste/HomeMealTaste.Data/Models/Order.cs
+++ HomeMealTaste/HomeMealTaste.Data/Models/Order.cs
@@ -5,6 +5,12 @@ namespace HomeMealTaste.Data.Models
 {
     public partial class Order
     {
+        public Order()
+        {
+            Payments = new HashSet<Payment>();
+            Transactions = new HashSet<Transaction>();
+        }
+
         public int OrderId { get; set; }
         public DateTime? Date { get; set; }
         public int? CustomerId { get; set; }
@@ -12,5 +18,11 @@ namespace HomeMealTaste.Data.Models
         public int? MealId { get; set; }
         public int? SessionId { get; set; }
         public int? PromotionPrice { get; set; }
+
+        public virtual Customer? Customer { get; set; }
+        public virtual Meal? Meal { get; set; }
+        public virtual Session? Session { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; }
     }
 }
