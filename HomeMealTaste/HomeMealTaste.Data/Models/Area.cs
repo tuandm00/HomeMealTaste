d497b2918c5b4e52f368edff6ead3b13f2aec257
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-07 09:55:54 +0700

    commit

--- HomeMealTaste/HomeMealTaste.Data/Models/Area.cs
+++ HomeMealTaste/HomeMealTaste.Data/Models/Area.cs
@@ -5,9 +5,17 @@ namespace HomeMealTaste.Data.Models
 {
     public partial class Area
     {
+        public Area()
+        {
+            Groups = new HashSet<Group>();
+        }
+
         public int AreaId { get; set; }
         public int? SessionId { get; set; }
         public string? Address { get; set; }
         public string? District { get; set; }
+
+        public virtual Session? Session { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
     }
 }
