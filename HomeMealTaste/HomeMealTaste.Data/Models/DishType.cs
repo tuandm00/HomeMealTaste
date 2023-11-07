4d3b68a994969de4b0e066e0aa0da34049ac8639
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-07 10:12:41 +0700

    modify

--- HomeMealTaste/HomeMealTaste.Data/Models/DishType.cs
+++ HomeMealTaste/HomeMealTaste.Data/Models/DishType.cs
@@ -5,8 +5,15 @@ namespace HomeMealTaste.Data.Models
 {
     public partial class DishType
     {
+        public DishType()
+        {
+            Dishes = new HashSet<Dish>();
+        }
+
         public int DishTypeId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+
+        public virtual ICollection<Dish> Dishes { get; set; }
     }
 }
