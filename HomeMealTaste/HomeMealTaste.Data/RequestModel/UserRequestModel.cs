1bcbde645ff50f9c70a8a278e1caefb9508721c0
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2023-11-25 12:22:18 +0700

    modify api login

--- HomeMealTaste/HomeMealTaste.Data/RequestModel/UserRequestModel.cs
+++ HomeMealTaste/HomeMealTaste.Data/RequestModel/UserRequestModel.cs
@@ -8,17 +8,11 @@ namespace HomeMealTaste.Data.RequestModel
 {
     public class UserRequestModel
     {
-        public string? Name { get; set; }
-        public string? Username { get; set; }
+        
         public string? Password { get; set; }
-        public string? Email { get; set; }
+        
         public string? Phone { get; set; }
-        public string? Address { get; set; }
-        public string? Street { get; set; }
-        public string? Ward { get; set; }
-        public string? District { get; set; }
-        public int? Role { get; set; }
-        public bool? Status { get; set; }
+        
 
     }
 }
