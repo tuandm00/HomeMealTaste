31a304d053db865fdd53404505ede3baff308c91
tuandm00 <66359864+tuandm00@users.noreply.github.com>
2024-01-15 21:43:30 +0700

    modify api UpdateSessionAndAreaInSession

--- HomeMealTaste/HomeMealTaste.Data/ResponseModel/UpdateSessionAndAreaInSessionResponseModel.cs
+++ HomeMealTaste/HomeMealTaste.Data/ResponseModel/UpdateSessionAndAreaInSessionResponseModel.cs
@@ -14,11 +14,9 @@ namespace HomeMealTaste.Data.ResponseModel
         public string? EndTime { get; set; }
         public string? EndDate { get; set; }
         public int? UserId { get; set; }
-        public bool? Status { get; set; }
+        public string? Status { get; set; }
         public string? SessionType { get; set; }
         public string? SessionName { get; set; }
-        public bool? RegisterForMealStatus { get; set; }
-        public bool? BookingSlotStatus { get; set; }
         public List<int>? AreaIds { get; set; }
     }
 }
